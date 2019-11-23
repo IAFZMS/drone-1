b9dea375b06c96aa763109385646cd61826fa854
edgarhernandez94 <edgar_hdez94@hotmail.com>
2019-11-23 11:49:36 -0600

    Modelos Actualizados

--- my_web/my_web/drones/models.py
+++ my_web/my_web/drones/models.py
@@ -1,17 +1,32 @@
 from django.db import models
+TIPO_CHOICES = [
+    ('F', 'Ala Fija'),
+    ('R', 'Ala Rotatoria')
+]
+
+CAMARA_CHOICES = [
+    ('I', 'INCLUIDA'),
+    ('NI', 'NO INCLUIDA')
+]
+
+
 class Dron(models.Model):
-    modelo = models.CharField(max_length=400, null=False, blank=False)
-    año = models.CharField(max_length=400, null=False, blank=False, default="")
+    Tipo_de_dron= models.CharField(max_length=400, null=False, blank=False,default="----",choices=TIPO_CHOICES)
+    modelo = models.CharField(max_length=400, null=False, blank=False, default="")
+    autonomia = models.CharField(max_length=400, null=False, blank=False, default="")
+    distancia = models.CharField(max_length=400, null=False, blank=False, default="")
+    costo = models.CharField(max_length=400, null=False, blank=False, default="")
+    camara_dron=models.CharField(max_length=400, null=False, blank=False,default="",choices=CAMARA_CHOICES)
 
     def __str__(self):
-        return "{} {}".format(self.modelo, self.año)
+        return "Modelo: {}".format(self.modelo, self.año, self.autonomia, self.distancia, self.costo)
 
 class camara(models.Model):
-    modelo1 = models.CharField(max_length=400, null=False, blank=False)
-    año1 = models.CharField(max_length=400, null=False, blank=False, default="")
-
+    Sensor = models.CharField(max_length=400, null=False, blank=False)
+    Calidad = models.CharField(max_length=400, null=False, blank=False, default="")
+    Objetivo= models.CharField(max_length=400, null=False, blank=False, default="")
+    Resolucion= models.CharField(max_length=400, null=False, blank=False, default="")
+    Rango_ISO= models.CharField(max_length=400, null=False, blank=False, default="")
+    Velocidad_obturacion= models.CharField(max_length=400, null=False, blank=False, default="")
     def __str__(self):
-        return "{} {}".format(self.modelo1, self.año1)
-
-
-   
+        return "Sensor:{} Calidad:{} Objetivo: {} Resolucion: {} Rango Iso:{} Velocidad de obturación:{}".format(self.Sensor, self.Calidad, self.Objetivo, self.Resolucion, self.Rango_ISO, self.Velocidad_obturacion)
